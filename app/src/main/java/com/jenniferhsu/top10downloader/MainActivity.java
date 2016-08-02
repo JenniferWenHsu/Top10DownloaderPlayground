e69c9efd27c7fdf2ec6c0304e3df118388bb0584
JenniferWenHsu <jwhsu@berkeley.edu>
2016-08-02 20:13:41 +0800

    some changes 1

--- app/src/main/java/com/jenniferhsu/top10downloader/MainActivity.java
+++ app/src/main/java/com/jenniferhsu/top10downloader/MainActivity.java
@@ -4,6 +4,10 @@ import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.ViewDebug;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -11,69 +15,77 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
+// This activity downloads XML files from RSS field.
 public class MainActivity extends AppCompatActivity {
+
+    private TextView xmlTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        DownloadData downloadData = new DownloadData();
+        downloadData.execute("http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topfreeapplications/limit=10/xml");
+
+        xmlTextView = (TextView) findViewById(R.id.xmlTextView);
+
+        // add some change 1
     }
 
-    // Async Task.
-    // String: Download location
-    // Second parameter: usually you want to use this for the progress bar
-    // Thrid parameter: actual result
-    private class DownloadData extends AsyncTask<String, Void, String> {
+    private class DownloadData extends AsyncTask<String, Void, String>{
+        private String TAG="DataDownload";
+        private String mFileContent;
 
-        private String TAG="DownloadData";
-
-        private String mFileContents;
-        // String... means that the number of the parameters is a variable.
         @Override
         protected String doInBackground(String... params) {
-            // tasks that you want to run in the background
-            mFileContents = downloadXMLFile(params[0]);
-            if (mFileContents == null){
-                Log.d(TAG, "Error downloading!");
+            mFileContent = downloadXML(params[0]);
+            if(mFileContent == null){
+                Log.d(TAG, "Error downloading");
             }
-
-            return mFileContents;
+            return mFileContent;
         }
 
-        // Write the code that does the actual downloading.
-        private String downloadXMLFile(String urlPath){
-            // Create a temporary buffer to store all those files
-            // because we can only download so much while downloading.
+        // Once doInBackground is finished, it will call this method.
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            Log.d(TAG, "Result was: "+result);
+            xmlTextView.setText(mFileContent);
+        }
+
+        private String downloadXML(String urlPath){
+            // A more efficient way of putting strings together.
             StringBuilder tempBuffer = new StringBuilder();
             try{
+                // Given the urlPath, you want to create connection to that URL
                 URL url = new URL(urlPath);
                 HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-
-                // An example would be the 404 you get from a webpage
                 int response = connection.getResponseCode();
-                Log.d(TAG, "The respond code is: "+response);
+                Log.d(TAG, "The response code was: "+response);
                 InputStream is = connection.getInputStream();
                 InputStreamReader isr = new InputStreamReader(is);
 
                 int charRead;
                 char[] inputBuffer = new char[500];
+
                 while(true){
-                    // Try to read the datat in the inputBuffer.
-                    // Returns the number it's reading.
                     charRead = isr.read(inputBuffer);
-                    if(charRead <= 0) {
+                    if(charRead <= 0){
                         break;
                     }
                     tempBuffer.append(String.copyValueOf(inputBuffer, 0, charRead));
                 }
                 return tempBuffer.toString();
-
             } catch (IOException e){
-                Log.d(TAG, "IO Exception reading: "+e.getMessage());
+                Log.d(TAG, "Error: "+e.getMessage());
+                //e.printStackTrace();
+            } catch (SecurityException e){
+                Log.d(TAG, "Security exception. Needs permission"+e.getMessage());
             }
 
-            //REMEMBER TO GET RID OF THIS LINE!!!!!
-            return tempBuffer.toString();
+            // If it returns an error, then the method should still return null.
+            return null;
         }
     }
 
